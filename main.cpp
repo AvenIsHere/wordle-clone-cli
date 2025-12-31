4c54310742216dbec70469a335083a52c7b9dd0a
Aven <128219717+AvenIsHere@users.noreply.github.com>
2025-12-31 14:37:57 +0000

    first pass at making the code object-oriented. For now, everything is practically the same, but its in a class now

--- main.cpp
+++ main.cpp
@@ -26,66 +26,42 @@ const string C_RED    = "\033[31m";
 
 enum Status {GREEN, YELLOW, GRAY};
 
-size_t write_data(void *buffer, size_t size, size_t nmemb, void *userp) {
-    size_t returnSize = size * nmemb;
+class WordleGame {
 
-    static_cast<string *>(userp)->append(static_cast<char *>(buffer), returnSize);
+    string word;
 
-    return returnSize;
-}
+    static bool IsValidGuess(const string& guess, vector<string> guesses) {
+        if (guess.size() != WORD_LEN) {
+            cout << format("Guess must be {} characters long.", WORD_LEN) << endl;
+            return false;
+        }
+        if (std::ranges::find(guesses, guess) != guesses.end()) {
+            cout << "You have already guessed " << guess << "!" << endl;
+            return false;
+        }
+        return true;
+    }
 
-vector<string> GetWords() {
-    vector<string> words;
-    string tempBuffer;
-
-    CURL* handle = curl_easy_init();
-    curl_easy_setopt(handle, CURLOPT_URL, format("https://random-word-api.herokuapp.com/word?number={}&length={}", WORD_COUNT, WORD_LEN).c_str());
-    curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, write_data);
-    curl_easy_setopt(handle, CURLOPT_WRITEDATA, &tempBuffer);
-    CURLcode success = curl_easy_perform(handle);
-    if (success == CURLE_OK) {
-        try {
-            json j = json::parse(tempBuffer);
-            for (int i = 0; i < j.size(); i++) {
-                words.push_back(j[i]);
+    static void PrintGuess(const Status guessStatus[], const string& guess) {
+        for (int i = 0; i < WORD_LEN; i++) {
+            if (guessStatus[i] == GREEN) {
+                cout << C_GREEN << guess[i] << C_RESET;
+            } else if (guessStatus[i] == YELLOW) {
+                cout << C_YELLOW << guess[i] << C_RESET;
+            } else {
+                cout << guess[i];
             }
-        } catch (json::parse_error &e) {
-            cerr << "JSON parse error: " << e.what() << endl;
         }
+        cout << endl;
     }
-    curl_easy_cleanup(handle);
-    return words;
-}
 
-bool IsValidGuess(const string& guess, vector<string> guesses) {
-    if (guess.size() != WORD_LEN) {
-        cout << format("Guess must be {} characters long.", WORD_LEN) << endl;
-        return false;
+public:
+
+    explicit WordleGame(const string &givenWord) {
+        word = givenWord;
     }
-    if (std::ranges::find(guesses, guess) != guesses.end()) {
-        cout << "You have already guessed " << guess << "!" << endl;
-        return false;
-    }
-    return true;
-}
 
-void PrintGuess(const Status guessStatus[], const string& guess) {
-    for (int i = 0; i < WORD_LEN; i++) {
-        if (guessStatus[i] == GREEN) {
-            cout << C_GREEN << guess[i] << C_RESET;
-        } else if (guessStatus[i] == YELLOW) {
-            cout << C_YELLOW << guess[i] << C_RESET;
-        } else {
-            cout << guess[i];
-        }
-    }
-    cout << endl;
-}
-
-int main() {
-    curl_global_init(CURL_GLOBAL_ALL);
-
-    for (vector<string> words = GetWords(); const auto& word : words) {
+    void play() {
         bool guessed = false;
         vector<string> guesses;
         map<char, int> letterFreqPerm;
@@ -141,7 +117,47 @@ int main() {
                 guessed = false;
             }
         } while (!guessed);
+    }
 
+};
+
+static size_t write_data(void *buffer, size_t size, size_t nmemb, void *userp) {
+    size_t returnSize = size * nmemb;
+
+    static_cast<string *>(userp)->append(static_cast<char *>(buffer), returnSize);
+
+    return returnSize;
+}
+
+static vector<string> GetWords() {
+    vector<string> words;
+    string tempBuffer;
+
+    CURL* handle = curl_easy_init();
+    curl_easy_setopt(handle, CURLOPT_URL, format("https://random-word-api.herokuapp.com/word?number={}&length={}", WORD_COUNT, WORD_LEN).c_str());
+    curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, write_data);
+    curl_easy_setopt(handle, CURLOPT_WRITEDATA, &tempBuffer);
+    CURLcode success = curl_easy_perform(handle);
+    if (success == CURLE_OK) {
+        try {
+            json j = json::parse(tempBuffer);
+            for (int i = 0; i < j.size(); i++) {
+                words.push_back(j[i]);
+            }
+        } catch (json::parse_error &e) {
+            cerr << "JSON parse error: " << e.what() << endl;
+        }
+    }
+    curl_easy_cleanup(handle);
+    return words;
+}
+
+int main() {
+    curl_global_init(CURL_GLOBAL_ALL);
+
+    for (vector<string> words = GetWords(); const auto& word : words) {
+        WordleGame game(word);
+        game.play();
     }
 
     curl_global_cleanup();
